7e414425d905b975fe960e8cea7652a81f3d0d6d
Crew Spence <csspence123@gmail.com>
2020-09-04 05:42:53 -0500

    Solved for naive tests.

--- simple_directions_reversal_codewars.js
+++ simple_directions_reversal_codewars.js
@@ -5,6 +5,28 @@ solve(["Begin on Road A","Right on Road B","Right on Road C","Left on Road D"]) 
 solve(['Begin on Lua Pkwy', 'Right on Sixth Alley', 'Right on 1st Cr']) =  ['Begin on 1st Cr', 'Left on Sixth Alley', 'Left on Lua Pkwy']
 */
 
-function solve(arr){
-    //..
+const solve = (arr) => {
+  let array = [];
+  if(arr.length === 1) {
+      return arr;
+  }
+  for(let i = arr.length - 1; i > -1; i--) {
+    if(i === arr.length - 1) {
+        if(arr[i][0] === 'L') {
+            array.push('Begin' + arr[i].slice(4));
+        }
+        if(arr[i][0] === 'R') {
+            array.push('Begin' + arr[i].slice(5));
+        }
+        continue;
+    }
+    if(arr[i + 1][0] === 'L') {
+        array.push('Right' + arr[i].slice(5))
+    }
+    if(arr[i + 1][0] === 'R') {
+        array.push('Left' + arr[i].slice(5))
+    }
+  }
+
+  return array;
 }
